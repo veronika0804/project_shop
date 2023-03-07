e49cfe94efa344160ca9a110800fb0ebefe492fe
veronika0804 <80108188+veronika0804@users.noreply.github.com>
2023-03-07 21:23:57 +0300

    Create main.py

---
+++ project/scripts/psycopg/main.py
@@ -0,0 +1,7 @@
+import psycopg2
+
+with psycopg2.connect(database='org_mipt_atp_db', user='postgres', password='postgres', host='docker', port=49154) as conn:
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM PERSON")
+    print(cur.fetchall())
+
